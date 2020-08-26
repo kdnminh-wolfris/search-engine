72d6ba6ed41e4717cc9c6b05c544f623ccd5481f
yufeng1707 <htnphuong19@apcs.vn>
2020-08-26 22:00:36 +0700

    nothing much

--- trie.h
+++ trie.h
@@ -27,10 +27,8 @@ public:
 	// data of a file are keywords and their frequency
 
 	void save(string filename);
-	//save trie to filename in folder cheatsheet. format: <word> <frequency>
 
 	void load(string filename);
-	//load a vector <pair <string, int> > in filename from folder cheatsheet
 
 	void clear();
 	// clear nodes

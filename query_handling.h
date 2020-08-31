a4162c903ad44e987d717e5f927088056a744d24
Huỳnh Phạm Tân Thông <63225882+huynhphamtanthong@users.noreply.github.com>
2020-08-31 17:14:53 +0700

    Task 9 (op OR) + Task 10(op INTITLE) -Done

--- query_handling.h
+++ query_handling.h
@@ -6,6 +6,10 @@
 class QueryHandling {
 public:
 	void filter(string& search_string);
+	vector<pair<string, int>> OR(string search_string);
+	string intitle(string search_string);
+	string exclude(string search_string);
+	string price();
 };
 
 void QueryHandling::filter(string& search_string)
@@ -80,4 +84,93 @@ void QueryHandling::filter(string& search_string)
 	delete[]arr;
 }
 
+vector<pair<string, int>> QueryHandling::OR(string search_string)
+{
+	vector<pair<string, int>> tmp;
+	int pos = 0;
+	int num = 0;
+	for (int i = 0;i < search_string.length();++i)
+	{
+		if (search_string[pos] == ' ')
+		{
+			pos++;
+			continue;
+		}
+		if (search_string[i] == ' ')
+		{
+			if (search_string[pos] == 'O' && search_string[pos + 1] == 'R' && i - pos == 2)
+			{
+				pos = i + 1;
+				continue;
+			}
+			else
+			{
+				num++;
+				tmp.push_back(make_pair(search_string.substr(pos, i - pos), num));
+				pos = i + 1;
+			}
+		}
+		if (search_string[i + 1] == '\0')
+		{
+			num++;
+			tmp.push_back(make_pair(search_string.substr(pos, i - pos + 1), num));
+		}
+	}
+	return tmp;
+}
+
+string QueryHandling::intitle(string search_string)
+{
+	for (int i = 0;i < search_string.length() - 8;++i)
+	{
+		if (search_string.substr(i, 8) == "intitle:" && (search_string[i + 8] != '\0' && search_string[i + 8] != ' ') && (i == 0 || (i > 0 && search_string[i - 1] == ' ')))
+		{
+			int pos = 0; // end position of intitle
+			for (int j = i + 9; j <= search_string.length();++j)
+			{
+				if (search_string[j] == '\0' || search_string[j] == ' ')
+				{
+					pos = j;
+					break;
+				}
+			}
+			if (i == 0)
+			{
+				if (search_string[pos] == '\0')
+				{
+					search_string.erase(i, pos);
+				}
+				else if (search_string[pos] == ' ')
+				{
+					search_string.erase(i, pos + 1);
+				}
+			}
+			else
+			{
+				search_string.erase(i - 1, pos - i + 1);
+			}
+			i--;
+		}
+		if (i >= search_string.length())
+			break;
+	}
+	return search_string;
+}
+
+string QueryHandling::exclude(string search_string) // redo
+{
+	for (int i = 0;i < search_string.length() - 1;++i)
+	{
+		if (search_string[i] == '-' && search_string[i + 1] == ' ' && search_string[i - 1] == ' ')
+		{
+			return search_string.substr(0, i - 1);
+		}
+	}
+	return search_string;
+}
+
+string QueryHandling::price()
+{
+
+}
 #endif

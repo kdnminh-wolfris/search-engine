9789516876f901b76f478634764b63bb49e2be1e
nhhung1810 <59091317+yauangon@users.noreply.github.com>
2020-09-09 11:47:13 +0700

    intersection

--- search.cpp
+++ search.cpp
@@ -18,19 +18,22 @@ void firstSearch::exit()
 	return;
 }
 
-void firstSearch::intersection(vector<string> f2)
+void firstSearch::intersection(vector<pair<string, int>> f2)
 {
 	if (!this->container || this->container->size() == 0)
 	{
-		if (!this->container) delete this->container;
-		this->exit();
+		if (!this->container) this->container = new map<string, int>;
+		for (auto it = f2.begin(); it != f2.end(); it++)
+		{
+			(*this->container)[it->first] = it->second;
+		}
 		return;
 	}
 	//HAVE NOT OPTIMIZED YET
-	map<string, bool>* newContainer = new map<string, bool>;
+	map<string, int>* newContainer = new map<string, int>;
 	for (auto it = f2.begin(); it != f2.end(); it++)
 	{
-		auto temp = this->container->find(*it);
+		auto temp = this->container->find((*it).first);
 		if (temp == this->container->end()) continue;
 		(*newContainer)[temp->first] = true;
 	}
@@ -40,17 +43,16 @@ void firstSearch::intersection(vector<string> f2)
 	return;
 }
 
-vector<string> firstSearch::intersection(vector<string>* f1, vector<string>* f2)
+vector<pair<string, int>> firstSearch::intersection(vector<pair<string, int>>* f1, vector<pair<string, int>>* f2)
 {
 	//code goes here;
-	vector<string> result;
+	vector<pair<string, int>> result;
 	return result;
 }
-					
-vector<string> firstSearch::search(string ss, Trie* T)
+
+vector<pair<string, int>> firstSearch::search(string ss, Trie* T)
 {
-	if (!T) return vector<string>();
-	return vector<string>();
+	return vector<pair<string, int>>();
 }
 
 string firstSearch::matchSearch(vector<string> quotes, string filename)
@@ -85,37 +87,30 @@ string firstSearch::matchSearch(vector<string> quotes, string filename)
 	return filename;
 }
 
-public:
+
 //SS STAND FOR SEARCH_STRING
-void firstSearch::intitle(string ss)
+vector<pair<string, int>> firstSearch::intitle(string ss)
 {
-	//LOAD A TRIE OF FILE
-	vector<string> f2 = this->search(ss, fileList);
-	for (auto it = f2.begin(); it != f2.end(); it++)
-	{
-		(*this->container)[*it] = true;
-	}
-	this->gate++;
 	return;
 }
 
-void firstSearch::quote(vector<pair<string, int>> quotes)
+vector<pair<string, int>> firstSearch::quote(vector<pair<string, int>> quotes)
 {
 	if (!quotes.size()) return;
 
 	string first = quotes[0].first;
 	vector<string> q;
 	/*CONSTRUCT THE FULL QUOTE
-
-
 	END CONSTRUCTING THE FULL QUOTE*/
-	vector<string> tempResult = this->search(first, this->T);
-	vector<string>* result = new vector<string>;
-	for (vector<string>::iterator it = tempResult.begin(); it != tempResult.end(); it++)
+	vector<pair<string, int>> tempResult = this->search(first, this->T);
+	vector<pair<string, int>>* result = new vector<pair<string, int>>;
+	for (auto it = tempResult.begin(); it != tempResult.end(); it++)
 	{
-		string filename = this->matchSearch(q, *it);
+		//CHANGE RANK AFTER
+		int RANK = 10;
+		string filename = this->matchSearch(q, (*it).first);
 		if (!filename.size()) continue;
-		(*result).push_back(filename);
+		(*result).push_back(make_pair(filename, RANK));
 	}
 
 	this->intersection(*result);
@@ -124,18 +119,18 @@ void firstSearch::quote(vector<pair<string, int>> quotes)
 	return;
 }
 
-void firstSearch::origin(vector<pair<string, int>> origins)
+vector<pair<string, int>> firstSearch::origin(vector<pair<string, int>> origins)
 {
-	vector<string>* result = nullptr;
+	vector<pair<string, int>>* result = nullptr;
 	for (vector<pair<string, int>>::iterator it = origins.begin(); it != origins.end(); it++)
 	{
 		if (!result)
 		{
-			result = new vector<string>;
+			result = new vector<pair<string, int>>;
 			(*result) = this->search((*it).first, this->T);
 			continue;
 		}
-		vector<string>* temp = new vector<string>;
+		vector<pair<string, int>>* temp = new vector<pair<string, int>>;
 		(*temp) = this->search((*it).first, this->T);
 		(*result) = this->intersection(result, temp);
 		delete temp;
@@ -147,16 +142,16 @@ void firstSearch::origin(vector<pair<string, int>> origins)
 	return;
 }
 
-void firstSearch::findOr(vector<pair<string, int>> OR)
+vector<pair<string, int>> firstSearch::findOr(vector<pair<string, int>> OR)
 {
 	this->gate++;
-	return;
+	return vector<pair<string, int>>();
 }
 
-void firstSearch::exclude(string ss)
+vector<pair<string, int>> firstSearch::exclude(string ss)
 {
 	this->gate++;
-	return;
+	return vector<pair<string, int>>();
 }
 
 firstSearch::firstSearch(string query, Trie* T)
@@ -164,11 +159,3 @@ firstSearch::firstSearch(string query, Trie* T)
 	if (!this->T) this->T = T;
 	this->query = query;
 }
-
-firstSearch::firstSearch(string query, Trie* T, Trie* fileList)
-{
-	if (!this->T) this->T = T;
-	if (!this->fileList) this->fileList = fileList;
-	this->query = query;
-}
-};

3c3d1a5a6b6325a3bb931df57aad5c1bfaf01906
yufeng1707 <htnphuong19@apcs.vn>
2020-09-16 20:07:38 +0700

    sửa bug load và save

--- search engine.cpp
+++ search engine.cpp
@@ -12,13 +12,28 @@
 using namespace std;
 
 int main() {
+	/*TRIE LOAD TEST*//*
 	Trie T;
-	T.load("save");
-	vector<pair<string, int>> temp = T.search("Buddhists");
 
-	for (auto it = temp.begin(); it != temp.end(); it++)
+	T.load("save");
+
+	T.trieTraverse();
+	*/
+
+	/*FILE FEEDING AND TRIE SAVING TEST*/
+	File_Handling FH;
+	Trie T = FH.head;
+
+
+	/*TRIE SEARCHING TEST*/
+	string tmp;
+	do
 	{
-		cout << it->first << " " << it->second << endl;
-	}
-	return 0;
+		cin >> tmp;
+		cout << "Search result for: " << tmp << endl;
+		vector<pair<string, int>> result = T.search(tmp);
+		for (auto it = result.begin(); it != result.end(); it++)
+			cout << it->first << " " << it->second << endl;
+	} while (tmp != "exit");
+	return 0;	
 }

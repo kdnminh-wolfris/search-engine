9a5fda5d552252715474aa0610bacdd6b32b301b
yufeng1707 <htnphuong19@apcs.vn>
2020-09-17 13:55:54 +0700

    range and price

--- search.h
+++ search.h
@@ -51,6 +51,10 @@ public:
 
 	vector <pair <string, int>> filetype(string type);
 
+	vector<pair<string, int>> number_searching(int lower, int upper, bool is_price);
+
+	vector<pair<string, int>> price_searching(string object, int price);
+
 	firstSearch(string query, Trie T);
 };
 #endif
